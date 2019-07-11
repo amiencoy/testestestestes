af90b107ba46ee95cae34194201733644356d18c
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-12 00:09:16 +0700

    Update db_connect.php

--- new2/db_connect.php
+++ new2/db_connect.php
@@ -3,7 +3,7 @@
 $host = "testestes.database.windows.net"; 
 $username = "xperimental"; 
 $password = "Bandar05"; 
-$dbname = "testestes"; 
+$dbname = "mahasiswa"; 
 $connect = new mysqli($host, $username, $password, $dbname); 
 if($connect->connect_error) {
     die("connection failed : " . $connect->connect_error);
