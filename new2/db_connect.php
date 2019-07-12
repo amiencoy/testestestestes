5b492ba41a27f58334dff9b5d6ecc38e134b683d
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-12 17:39:54 +0700

    Update db_connect.php

--- new2/db_connect.php
+++ new2/db_connect.php
@@ -1,10 +1,9 @@
 <?php 
  
-$localhost = "xperimental@testestes.mysql.database.azure.com"; 
-$username = "xperimental"; 
+$localhost = "xperimental@testestes.mysql.database.azure.com";
 $password = "Bandar05"; 
 $dbname = "mahasiswa"; 
-$connect = new mysqli($localhost, $username, $password, $dbname); 
+$connect = new mysqli($localhost, $password, $dbname); 
 if($connect->connect_error) {
     die("connection failed : " . $connect->connect_error);
 }
