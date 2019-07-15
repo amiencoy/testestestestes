031b5053df84a7301bece9baddea4aa1f552cb74
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-15 21:34:12 +0700

    Update db_connect.php

--- new2/db_connect.php
+++ new2/db_connect.php
@@ -1,5 +1,6 @@
 <?php 
- 
+
+/*
 $localhost = "xperimental@testestes.mysql.database.azure.com";
 $username = "xperimental";
 $password = "Bandar05"; 
@@ -8,5 +9,20 @@ $connect = new mysqli($localhost, $username, $password, $dbname);
 if($connect->connect_error) {
     die("connection failed : " . $connect->connect_error);
 }
- 
+*/ 
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:testestes.database.windows.net,1433; Database = mahasiswa", "xperimental", "{Bandar05}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "xperimental@testestes", "pwd" => "{Bandar05}", "Database" => "mahasiswa", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testestes.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
