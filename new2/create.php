8bc0de2ed757695b383717a4736608bcfbf629da
Muhammad Amien <muhammad.18017@mhs.unesa.ac.id>
2019-07-15 22:29:45 +0700

    Update create.php

--- new2/create.php
+++ new2/create.php
@@ -6,7 +6,7 @@ if($_POST) {
     $NIM = $_POST['NIM'];
     $nama = $_POST['nama'];
     $prodi = $_POST['prodi'];
-    $sql = "INSERT INTO mahasiswa (NIM, nama, prodi) VALUES ('$NIM', '$nama', '$prodi')";
+    $sql = "INSERT INTO [dbo].[mahasiswa] (NIM, nama, prodi) VALUES ('$NIM', '$nama', '$prodi')";
     if($connect->query($sql) === TRUE) {
         echo "<p>data berhasil ditambahkan</p>";
         echo "<a href='../create.php'><button type='button'>kembali</button></a>";
